275928d205df46b08a52572a2bb01a2bc9b0eebe
Yojal-karki7 <yojalkarki@gmail.com>
2025-02-03 12:42:09 +0545

    siii

--- portfolio/src/pages/sub-components/Hero.jsx
+++ portfolio/src/pages/sub-components/Hero.jsx
@@ -45,7 +45,7 @@ const Hero = () => {
             Github
           </span>
           </Button></Link>
-        <Link>
+        <Link to={user.resume && user.resume.url} target='_blank'>
         <Button className='rounded-[30px] flex items-center gap-2 flex-row'>
           <span>
             <ExternalLink />
